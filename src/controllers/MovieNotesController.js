8f295f0e3dfc7b530124668a6f076dd8f44a9788
Vinícius <santsmarcos81@gmail.com>
2024-03-09 14:33:33 -0300

    Inserção da listagem e filtro de notas

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -20,9 +20,11 @@ class MovieNotesController {
         const { id } = request.params;
 
         const note = await knex("movie_notes").where({ id }).first();
+        const tags = await knex("movie_tags").where({ note_id: id }).orderBy("name");
 
         return response.json({
-            ...note
+            ...note,
+            tags
         });
     }
 
@@ -33,6 +35,45 @@ class MovieNotesController {
 
         return response.json();
     }
+
+    async index(request, response) {
+        const { title, user_id, tags } = request.query;
+
+        let notes;
+
+        if(tags){
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("movie_tags")
+                .select([
+                    "movie_notes.id",
+                    "movie_notes.title",
+                    "movie_notes.user_id"
+                ])
+                .where("movie_notes.user_id", user_id)
+                .whereLike("movie_notes.title", `%${title}%`)
+                .whereIn("name", filterTags)
+                .innerJoin("movie_notes","movie_notes.id","movie_tags.note_id")
+                .orderBy("movie_notes.title");
+        } else {
+            notes = await knex("movie_notes")
+                .where({ user_id })
+                .whereLike("title", `%${title}%`)
+                .orderBy("title")
+        }
+
+        const userTags = await knex("movie_tags").where({ user_id });
+        const notesWhithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        });
+
+        return response.json(notesWhithTags);
+    } 
 }
 
 module.exports = MovieNotesController;
--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -9,5 +9,6 @@ const movieNotesController = new MovieNotesController();
 movieNotesRoutes.post("/:user_id", movieNotesController.create);
 movieNotesRoutes.get("/:id", movieNotesController.show);
 movieNotesRoutes.delete("/:id", movieNotesController.delete);
+movieNotesRoutes.get("/", movieNotesController.index);
 
 module.exports = movieNotesRoutes;
